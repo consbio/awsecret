6eb6b95390c95186db5cdd6950cc33ad0bc23ad4
nikmolnar <nik.molnar@consbio.org>
2015-09-27 14:50:24 -0700

    Fix setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     keywords='password,aws,s3,credentials,encryption',
     version=VERSION,
     packages=['awsecret'],
-    requires=['pycrypto', 'boto', 'click'],
+    install_requires=['pycrypto', 'boto', 'click'],
     url='https://github.com/consbio/awsecret',
     license='BSD',
     entry_points={
